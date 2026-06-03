60ed72f021184d982c7017e315c12634e4f2593f
lrf22 <liurf1647@163.com>
2026-06-03 16:43:31 +0800

    fix(ssbuffer): fix attr name in SplitDataflow

--- third_party/ascend/lib/DynamicCVPipeline/SplitDataflow/DataDependencyAnalysis.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/SplitDataflow/DataDependencyAnalysis.cpp
@@ -49,16 +49,13 @@ using namespace mlir::triton;
 using namespace mlir::CVPipeline;
 
 // Attribute name constants
-static constexpr const char *kBlockIdAttr = "ssbuffer.block_id";
-static constexpr const char *kCoreTypeAttr = "ssbuffer.core_type";
-static constexpr const char *kTransferIdAttr = "ssbuffer.transfer_id";
 static constexpr const char *ssbufferCoreTypeCubeAttr = "CUBE";
 static constexpr const char *ssbufferCoreTypeVectorAttr = "VECTOR";
 
 // Helper: ssbuffer.core_type
 llvm::StringRef getSsbufferCoreType(Operation *op)
 {
-    if (auto attr = op->getAttrOfType<mlir::StringAttr>(kCoreTypeAttr)) {
+    if (auto attr = op->getAttrOfType<mlir::StringAttr>(CVPipeline::kCoreType)) {
         return attr.getValue();
     }
     return "";
@@ -264,8 +261,8 @@ void DataDependencyAnalysisPass::insertProducerAndRecordDeps(scf::ForOp forOp,
     builder.setInsertionPointToStart(&bodyBlock);
     Location loc = forOp.getLoc();
     auto constOp = builder.create<arith::ConstantIntOp>(loc, 0, 32);
-    constOp->setAttr(kBlockIdAttr, IntegerAttr::get(IntegerType::get(builder.getContext(), 32), newId));
-    constOp->setAttr(kCoreTypeAttr, StringAttr::get(builder.getContext(), initCoreType));
+    constOp->setAttr(CVPipeline::kBlockId, IntegerAttr::get(IntegerType::get(builder.getContext(), 32), newId));
+    constOp->setAttr(CVPipeline::kCoreType, StringAttr::get(builder.getContext(), initCoreType));
 
     for (auto &user : diffUsers) {
         auto userBlockIdOpt = CVPipeline::getOpBlockId(user);
