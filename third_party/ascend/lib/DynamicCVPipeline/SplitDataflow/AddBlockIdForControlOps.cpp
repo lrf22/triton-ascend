3591ff99511259386b63ef9297161ccdeca3807c
lrf22 <liurf1647@163.com>
2026-06-08 17:18:18 +0800

    [ssbuffer](fix) process yield op in scf.if

--- third_party/ascend/lib/DynamicCVPipeline/SplitDataflow/AddBlockIdForControlOps.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/SplitDataflow/AddBlockIdForControlOps.cpp
@@ -35,6 +35,7 @@ using namespace mlir::triton;
 // Pass Entry Point
 void AddBlockIdForControlOpsPass::runOnOperation()
 {
+  static constexpr int kIntegerBitWidth = 32;
   LOG_DEBUG("\n--- enter AddBlockIdForControlOpsPass --->\n");
   ModuleOp module = getOperation();
 
@@ -45,17 +46,35 @@ void AddBlockIdForControlOpsPass::runOnOperation()
 
   // Step 2: add block_id for control flow ops
   module.walk([&](Operation *op) {
+    
     // skip op with block_id
     if (op->getAttrOfType<IntegerAttr>("ssbuffer.block_id")) {
       return;
     }
 
     if (isa<scf::ForOp, scf::IfOp, scf::WhileOp>(op)) {
+      
       maxBlockId++;
-      static constexpr int kIntegerBitWidth = 32;
       op->setAttr("ssbuffer.block_id",
                   IntegerAttr::get(IntegerType::get(module.getContext(), kIntegerBitWidth), maxBlockId));
       LOG_DEBUG("Added block_id " << maxBlockId << " to " << *op << "\n");
+    }
+
+    // coretype of scf.yield may not be the same with defining op
+    if (isa<scf::YieldOp>(op) && isa<scf::IfOp>(op->getParentOp())) {
+      Operation *parentOp = op->getParentOp();
+      auto ifBlockIdOpt = CVPipeline::getOpBlockId(parentOp);
+
+      int yieldBlockId;
+      if (ifBlockIdOpt) {
+        yieldBlockId = static_cast<int>(*ifBlockIdOpt);
+      } else {
+        maxBlockId++;
+        yieldBlockId = maxBlockId;
+      }
+      op->setAttr("ssbuffer.block_id",
+                  IntegerAttr::get(IntegerType::get(module.getContext(), kIntegerBitWidth), yieldBlockId));
+      LOG_DEBUG("Added block_id " << yieldBlockId << " to " << *op << "\n");
     }
   });
 
