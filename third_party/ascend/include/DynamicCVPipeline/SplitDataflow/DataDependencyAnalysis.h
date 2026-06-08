d61312664400a9f839c07a2669fb862b23d3d1c0
lrf22 <liurf1647@163.com>
2026-06-08 10:27:20 +0800

    fix(ssbuffer): fix iterarg dependencies

--- third_party/ascend/include/DynamicCVPipeline/SplitDataflow/DataDependencyAnalysis.h
+++ third_party/ascend/include/DynamicCVPipeline/SplitDataflow/DataDependencyAnalysis.h
@@ -134,7 +134,9 @@ private:
                                                 int consumerBlockId);
 
     bool isControlFlowOp(mlir::Operation *op);
-    bool isValidTensorForDependency(mlir::Value value);
+    bool isCubeOrVectorOp(mlir::Operation *op);
+    bool isValidShapeForDependency(mlir::Value value);
+    bool isValidValueForDependency(mlir::Value value);
     bool isOuterOpArg(mlir::Value value);
     void processIterArgDependencies();
     llvm::SmallVector<mlir::Operation *> collectDiffCoreTypeUsers(
